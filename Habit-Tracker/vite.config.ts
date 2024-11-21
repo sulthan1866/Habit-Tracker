d0a89b6c4b1ca2416d64edfc435debc124ccfdb2
sulthan1866 <mohammed.sulthan.2972004@gmail.com>
2024-11-22 04:04:50 +0530

    update vite.config

--- Habit-Tracker/vite.config.ts
+++ Habit-Tracker/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // Other server configurations if needed
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+    },
+  },
+});
