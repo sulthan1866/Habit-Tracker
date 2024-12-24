998cb1d93c1893fcf9a11d3ea6d7368d8fe18eb1
Mohammed Sulthan <mohammed.sulthan.2972004@gmail.com>
2024-12-25 03:56:07 +0530

    Add files via upload

---
+++ habitTracker/target/classes/data.sql
@@ -0,0 +1,4 @@
+insert into users values ('wiirngeoerngieanfirtgesadgtyin',null,'e1','$2a$12$86IWINQawBSgjFgSTpBirOy1d3byCHQw9zsdgThhUPBIRsGHwBW');
+insert into users values ('wii',null,'e2','$2a$12$86IWINQawBSgjFgSTpBirOy1d3byCHQw9zsdgThhUPBIRsGHwBW/a');--pass
+insert into users values ('KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK',null,'e3','pass');
+insert into habit values (3,9,default,7,'lo',12,9,'wii');
